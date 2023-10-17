d952c50393f52e70ca42509fa22d1e99960298eb
GONZALO MORENO <gonzalo_moreno@keysight.com>
2023-10-18 01:30:12 +0200

    new messages

--- Source/Program.cs
+++ Source/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello new world");
+        Console.WriteLine("Hello 2");
     }
 }
