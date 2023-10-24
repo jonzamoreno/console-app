ab84366101831aa06442ec477ef53db8ec8f6ad9
GONZALO MORENO <gonzalo_moreno@keysight.com>
2023-10-25 00:43:37 +0200

    Clean-up main code

--- Source/Program.cs
+++ Source/Program.cs
@@ -5,8 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello new world");
-        Console.WriteLine("Hello 2");
-        Console.WriteLine("Hola Mundo");
-        Console.WriteLine("Hola Mundo");
     }
 }
